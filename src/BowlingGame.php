b8fa9e63fb78fa152c8c3926a0ebbc16f3c0f2cf
Syed Huda <syed.huda@gmail.com>
2017-01-12 20:10:30 +0000

    Test for 0 game.

---
+++ src/BowlingGame.php
@@ -0,0 +1,14 @@
+<?php
+
+class BowlingGame
+{
+    public function roll($roll)
+    {
+
+    }
+
+    public function score()
+    {
+        return 0;
+    }
+}
--- tests/BowlingGameTest.php
+++ tests/BowlingGameTest.php
@@ -3,8 +3,14 @@
 class BowlingGameTest extends PHPUnit_Framework_TestCase
 {
     /** @test */
-    public function it_returns_true()
-    {
-        $this->assertTrue(true);
+    function it_scores_a_gutter_game_as_zero() {
+        /** @var BowlingGame $game */
+        $game = new BowlingGame();
+        for ($i = 0; $i < 20; $i++) {
+            $game->roll(0);
+        }
+
+        $score = $game->score();
+        $this->assertEquals(0, $score);
     }
 }
